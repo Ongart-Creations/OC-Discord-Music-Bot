746371e8753a6d7cb92f7a48c8b6cf63c81f65d8
Ongart-Creations <33317286+Ongart-Creations@users.noreply.github.com>
2025-07-25 04:24:47 +0200

    added a config

---
+++ OC Discord Music Bot/Misc/Config/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCDiscordMusicBot.Misc.Config
+{
+    internal static class Config
+    {
+        public static string BotToken = string.Empty;
+    }
+}
